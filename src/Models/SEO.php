95974811348d30fc01582116ebf3566bf3003cf8
Anastas Mironov <ast@l80.ru>
2023-07-10 11:38:07 +0600

    feat: all seo fields is translatable

--- src/Models/SEO.php
+++ src/Models/SEO.php
@@ -3,12 +3,21 @@
 namespace AdminKit\SEO\Models;
 
 use AdminKit\Core\Abstracts\Models\AbstractModel;
+use Spatie\Translatable\HasTranslations;
 
 class SEO extends AbstractModel
 {
+    use HasTranslations;
+
     protected $table = 'admin_kit_seo';
 
     protected $fillable = [
+        'title',
+        'description',
+        'keywords',
+    ];
+
+    protected $translatable = [
         'title',
         'description',
         'keywords',
