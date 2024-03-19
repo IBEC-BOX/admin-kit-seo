4e382ff3d59a4569b6531a74189c798eb56d041c
daurensky <daurensky@users.noreply.github.com>
2024-03-19 08:50:37 +0000

    Fix styling

--- src/UI/Filament/Resources/PageSEOResource/Pages/CreatePageSEO.php
+++ src/UI/Filament/Resources/PageSEOResource/Pages/CreatePageSEO.php
@@ -2,13 +2,12 @@
 
 namespace AdminKit\SEO\UI\Filament\Resources\PageSEOResource\Pages;
 
-use Filament\Resources\Pages\CreateRecord;
 use AdminKit\SEO\UI\Filament\Resources\PageSEOResource;
+use Filament\Resources\Pages\CreateRecord;
 
 class CreatePageSEO extends CreateRecord
 {
     protected static string $resource = PageSEOResource::class;
-
 
     protected function getRedirectUrl(): string
     {
